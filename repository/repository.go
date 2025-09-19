268671daaec3530ba778284a3708355b1b3af0aa
Jeff Reiffers <jeffreiffers@gmail.com>
2025-09-19 12:15:51 +0200

    improve: more strict input validation

--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,6 +15,7 @@ import (
 	"github.com/Informasjonsforvaltning/fdk-resource-service/config/mongodb"
 	"github.com/Informasjonsforvaltning/fdk-resource-service/model"
 	"github.com/Informasjonsforvaltning/fdk-resource-service/utils/pointer"
+	"github.com/Informasjonsforvaltning/fdk-resource-service/utils/validate"
 	"github.com/sirupsen/logrus"
 )
 
@@ -95,6 +97,8 @@ func InitServiceRepository() *ResourceRepositoryImpl {
 }
 
 func (r ResourceRepositoryImpl) StoreResource(ctx context.Context, resource model.DBO) error {
+	// ID should already have been sanitized and validated by caller
+
 	var replaceOptions = options.Replace()
 	replaceOptions.Upsert = pointer.Of(true)
 
@@ -108,6 +112,7 @@ func (r ResourceRepositoryImpl) StoreResource(ctx context.Context, resource mode
 			return err
 		}
 
+		// Use parameterized query with validated ID
 		_, err = r.collection.ReplaceOne(sctx, bson.D{{Key: "_id", Value: resource.ID}}, resource, replaceOptions)
 		if err != nil {
 			sctx.AbortTransaction(sctx)
@@ -134,6 +139,11 @@ func (r ResourceRepositoryImpl) StoreResource(ctx context.Context, resource mode
 
 func (r ResourceRepositoryImpl) GetResources(ctx context.Context, query bson.D) ([]model.DBO, error) {
 	var resources []model.DBO
+
+	// Validate query to prevent injection attacks
+	if err := r.validateQuery(query); err != nil {
+		return resources, err
+	}
 
 	current, err := r.collection.Find(ctx, query)
 	if err != nil {
@@ -163,7 +173,13 @@ func (r ResourceRepositoryImpl) GetResources(ctx context.Context, query bson.D) 
 }
 
 func (r ResourceRepositoryImpl) GetResource(ctx context.Context, id string) (model.DBO, error) {
-	filter := bson.D{{Key: "_id", Value: id}}
+	// Validate and sanitize ID to prevent injection
+	sanitizedID, err := validate.SanitizeAndValidateID(id)
+	if err != nil {
+		return model.DBO{}, err
+	}
+
+	filter := bson.D{{Key: "_id", Value: sanitizedID}}
 	bytes, err := r.collection.FindOne(ctx, filter).Raw()
 
 	if err != nil {
@@ -177,4 +193,49 @@ func (r ResourceRepositoryImpl) GetResource(ctx context.Context, id string) (mod
 	}
 
 	return dbo, nil
+}
+
+// validateQuery validates MongoDB queries to prevent injection attacks
+func (r ResourceRepositoryImpl) validateQuery(query bson.D) error {
+	// Define allowed query operators and fields
+	allowedOperators := map[string]bool{
+		"$eq": true, "$ne": true, "$gt": true, "$gte": true, "$lt": true, "$lte": true,
+		"$in": true, "$nin": true, "$exists": true, "$regex": true, "$and": true, "$or": true,
+		"$not": true, "$nor": true, "$all": true, "$elemMatch": true, "$size": true,
+	}
+	
+	allowedFields := map[string]bool{
+		"_id": true, "deleted": true, "timestamp": true, "resource": true,
+	}
+
+	for _, elem := range query {
+		key := elem.Key
+		value := elem.Value
+
+		// Check if field is allowed
+		if !allowedFields[key] {
+			// Check if it's an operator
+			if !allowedOperators[key] {
+				return errors.New("disallowed field or operator in query: " + key)
+			}
+		}
+
+		// Recursively validate nested queries
+		if nestedQuery, ok := value.(bson.D); ok {
+			if err := r.validateQuery(nestedQuery); err != nil {
+				return err
+			}
+		}
+
+		// Validate array of queries (for $and, $or, etc.)
+		if nestedQueries, ok := value.([]bson.D); ok {
+			for _, nestedQuery := range nestedQueries {
+				if err := r.validateQuery(nestedQuery); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
 }
